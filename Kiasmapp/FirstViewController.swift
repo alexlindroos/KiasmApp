23b423bed5fe14fbf4fbd04d4176561daae6c103
Juhani Lavonen <juhani.lavonen.tuusula@gmail.com>
2016-04-10 16:44:18 +0300

    Initial Commit

---
+++ Kiasmapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Kiasmapp
+//
+//  Created by Juhani Lavonen on 10.4.2016.
+//  Copyright © 2016 Juhani Lavonen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
